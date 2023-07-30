d4776a14655448aa08c4e76a4fcf383243dbc542
WvdL <willem@wlagemaat.nl>
2023-07-30 17:07:43 +0200

    first start with amq

---
+++ vroem/src/main/java/nl/wlagemaat/demo/vroem/mq/MQConfig.java
@@ -0,0 +1,53 @@
+package nl.wlagemaat.demo.vroem.mq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class MQConfig {
+
+    @Value("${mq.topic.exchange}")
+    private String topicExchangeName;
+
+    @Value("${mq.queue}")
+    private String queueName;
+
+    @Value("${mq.routing.key}")
+    private String routingKey;
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+}
---
+++ vroem/src/test/java/nl/wlagemaat/demo/vroem/mq/MessageRabbitMQTest.java
@@ -0,0 +1,39 @@
+package nl.wlagemaat.demo.vroem.mq;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.amqp.AmqpConnectException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class MessageRabbitMQTest {
+
+    @MockBean
+    private MQRunner runner;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private Receiver receiver;
+
+    @Value("${mq.queue}")
+    private String queueName;
+
+    @Test
+    public void test() throws Exception {
+        try {
+            rabbitTemplate.convertAndSend(queueName, "Hello from RabbitMQ!");
+            receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        } catch (AmqpConnectException e) {
+            // ignore - rabbit is not running
+        }
+    }
+
+}
